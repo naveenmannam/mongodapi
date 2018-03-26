95e306bfb7e72a66350121e8eb0361ed37eef96e
Naveen Mannam <naveen.mannam@hotmail.com>
2018-03-26 17:54:21 -0400

    User model

--- server/server.js
+++ server/server.js
@@ -1,27 +1,43 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
-
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost:27017/TodoApp');
 // mongoose.connect(process.env.DB_URI);
-console.log(process.env.DB_URI)
 
-const Todo = mongoose.model('Todo', {
-  text:       {type: String},
-  completed:  {type: Boolean, default: false},
-  completeAt: {type: Number}
-});
+// const Todo = mongoose.model('Todo', {
+//   text:       {type: String, required: true, minlength: 1, trim: true},
+//   completed:  {type: Boolean, default: false, required: true},
+//   completeAt: {type: Number, default: null}
+// });
 
-const newTodo = new Todo({
-  text: 'Get some Avacadoes',
-  completed: false,
-  compleedAt: 4
+// const newTodo = new Todo({
+//   text: 'Get some Avacadoes',
+//   completed: false,
+//   compleedAt: 4
+// })
+
+// newTodo.save()
+//   .then(
+//     doc => console.log('Document save success', doc),
+//     err => console.log('Document save failed.')
+//   )
+//   .catch(err => console.log('Error'))
+
+const UserSchema = new mongoose.Schema({
+  name: {type: String, required: true},
+  email: {type: String, required: true},
+  age: {type: Number, default: 0}
 })
 
-newTodo.save()
-  .then(
-    doc => console.log('Document save success', doc),
-    err => console.log('Document save failed.')
-  )
-  .catch(err => console.log('Error'))
+const User = mongoose.model('User', UserSchema);
 
+const newUser = new User({
+  name: 'Minny Mannam',
+  email: 'minny.mannam@gmail.com',
+})
+
+newUser.save()
+  .then(
+    data => console.log('Record Saved', data),
+    err => console.log('Error saving record')
+  )
